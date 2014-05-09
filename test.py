9e67f29188a0cce286f39d057e697e1e7c67ca70
swannsonite <swannsonite@gmail.com>
2014-05-09 14:15:37 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import Diamond_subsidy
+
+print 'height 68,193 : 7,960.00000000'
+print Diamond_subsidy.GetBlockBaseValue(68193)
+print 'height 67223'
+print Diamond_subsidy.GetBlockBaseValue(67223)
+print 'height 77223'
+print Diamond_subsidy.GetBlockBaseValue(77223)
+print 'height 87223'
+print Diamond_subsidy.GetBlockBaseValue(87223)
+print 'height 97223'
+print Diamond_subsidy.GetBlockBaseValue(97223)
+print 'height 107223'
+print Diamond_subsidy.GetBlockBaseValue(107223)
+print 'height 1400'
+print Diamond_subsidy.GetBlockBaseValue(1400)
+print 'height 5750'
+print Diamond_subsidy.GetBlockBaseValue(5750)
